99a481c7237825f708d2c5b05770906b7edbcffb
Conzxy <Conzxy@1967933844@qq.com>
2022-07-10 13:39:51 +0800

    update

--- mmkv/algo/hash_set.h
+++ mmkv/algo/hash_set.h
@@ -1,16 +1,1 @@
-#ifndef _MMKV_ALGO_HASH_SET_H_
-#define _MMKV_ALGO_HASH_SET_H_
-
-#include "hash_util.h"
-#include "hash_table.h"
-
-namespace mmkv {
-namespace algo {
-
-template<typename K>
-using HashSet = HashTable<K, K, Hash<K>, GetKey<K>, EqualKey<K>, LibcAllocatorWithRealloc<K>>;
-
-} // algo
-} // mmkv
-
-#endif // _MMKV_ALGO_HASH_SET_H_
+#include "internal/hash_set_impl.h"
