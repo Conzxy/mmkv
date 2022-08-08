bff8d7905d4a2609e4d88da5ef8d8059dca0bb1a
Conzxy <1967933844@qq.com>
2022-08-08 10:36:34 +0800

    mru cache

---
+++ mmkv/replacement/internal/mru_cache_impl.h
@@ -0,0 +1,73 @@
+#ifndef MMKV_REPLACEMENT_INTERNAL_MRU_CACHE_IMPL_H_
+#define MMKV_REPLACEMENT_INTERNAL_MRU_CACHE_IMPL_H_
+
+#ifndef MMKV_REPLACEMENT_MRU_CACHE_H_
+#include "../mru_cache.h"
+#endif
+
+
+#define MRU_CACHE_TEMPLATE template<typename K>
+#define MRU_CACHE_CLASS MruCache<K>
+
+namespace mmkv {
+namespace replacement {
+
+MRU_CACHE_TEMPLATE
+template<typename U>
+auto MRU_CACHE_CLASS::UpdateEntry_(U &&entry) -> K* {
+  if (max_size_ == 0) return nullptr;
+
+
+  typename Dict::value_type *duplicate = nullptr;
+  auto success = dict_.InsertKvWithDuplicate(std::forward<U>(entry), nullptr, duplicate);
+  if (success) {
+    if (size() >= max_size_) {
+      DelVictim();
+    }
+
+    lst_.PushFront(&duplicate->key);
+    duplicate->value = lst_.FrontNode();
+  } else {
+    auto node = duplicate->value;
+    lst_.Extract(node);
+    lst_.PushFront(node);
+  }
+
+  return lst_.Front();
+}
+
+MRU_CACHE_TEMPLATE
+auto MRU_CACHE_CLASS::DelEntry(K const &key) -> bool {
+  auto entry = dict_.Extract(key);
+  if (entry) {
+    lst_.Erase(entry->value.value);
+    dict_.DropNode(entry);
+    return true;
+  }
+
+  return false;
+}
+
+MRU_CACHE_TEMPLATE
+auto MRU_CACHE_CLASS::Search(K const &key) -> K* {
+  auto ret = dict_.Find(key);
+  return ret ? &ret->key : nullptr;
+}
+
+MRU_CACHE_TEMPLATE
+auto MRU_CACHE_CLASS::DelVictim() -> void {
+  auto victim = lst_.Front();
+  dict_.Erase(*victim);
+  lst_.PopFront();
+}
+
+MRU_CACHE_TEMPLATE
+auto MRU_CACHE_CLASS::Clear() -> void {
+  dict_.Clear();
+  lst_.Clear();
+}
+
+} // replacement 
+} // mmkv
+
+#endif
---
+++ mmkv/replacement/mru_cache.h
@@ -0,0 +1,50 @@
+#ifndef MMKV_REPLACEMENT_MRU_CACHE_H_
+#define MMKV_REPLACEMENT_MRU_CACHE_H_
+
+#include <utility>
+
+#include "cache_interface.h"
+
+#include "mmkv/algo/dictionary.h"
+#include "mmkv/algo/blist.h"
+#include "mmkv/algo/libc_allocator_with_realloc_no_record.h"
+
+namespace mmkv {
+namespace replacement {
+ 
+template<typename K> 
+class MruCache : public CacheInterface<K> {
+  using List = algo::Blist<K*, algo::LibcAllocatorWithReallocNoRecord<K*>>;
+  using Dict = algo::Dictionary<K, typename List::Node*, algo::Hash<K>, algo::EqualKey<K>, algo::LibcAllocatorWithReallocNoRecord<K>>;
+ public:
+  explicit MruCache(size_t max_size)
+    : max_size_(max_size)
+  {
+  }
+  
+  auto UpdateEntry(K const &entry) -> K* override { return UpdateEntry_(entry); }
+  auto UpdateEntry(K &&entry) -> K* override { return UpdateEntry_(std::move(entry)); }
+  
+  template<typename U> 
+  auto UpdateEntry_(U &&entry) -> K*;
+
+  auto DelEntry(K const &key) -> bool override;
+  auto Search(K const &key) -> K* override;
+  auto Victim() -> K* override { return lst_.Front(); }
+  auto DelVictim() -> void override;
+  auto size() const noexcept -> size_t override { return lst_.size(); }
+  auto max_size() const noexcept -> size_t override { return max_size_; }
+  auto Clear() -> void override;
+  auto New() const -> MruCache* override { return new MruCache(max_size_); }
+ private:
+  size_t max_size_;
+  Dict dict_;
+  List lst_;
+};
+
+} // replacement
+} // mmkv
+
+#include "internal/mru_cache_impl.h"
+
+#endif // MMKV_REPLACEMENT_MRU_CACHE_H_
