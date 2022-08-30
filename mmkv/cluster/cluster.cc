2e927b2c4ddd074b252dc70e1f83ad628b251f6f
Conzxy <1967933844@qq.com>
2022-08-30 23:11:34 +0800

    test tool

---
+++ mmkv/cluster/cluster.cc
@@ -0,0 +1,35 @@
+#include "cluster.h"
+
+using namespace mmkv;
+using namespace mmkv::server;
+
+Cluster::Cluster(EventLoop *loop, InetAddr const &tracker_addr)
+  : tracker_(loop, tracker_addr)
+  , tracker_addr_(tracker_addr)
+  , tracker_cli_loop_thr_("TrackerClients")
+  , observer_loop_thr_("Observer")
+  , observer_(observer_loop_thr_.StartRun(),
+              InetAddr("127.0.0.1", tracker_addr.GetPort()))
+{
+  tracker_cli_loop_thr_.StartRun();
+}
+
+void Cluster::Start()
+{
+  tracker_.Listen();
+  observer_.Connect();
+}
+
+void Cluster::StartShardServer(InetAddr const &sharder_addr)
+{
+  tracker_clis_.emplace_back(new TrackerClient(
+      tracker_cli_loop_thr_.GetLoop(), tracker_addr_, sharder_addr,
+      "TrackerClient", "Sharder" + std::to_string(tracker_clis_.size())));
+  tracker_clis_.back()->Connect();
+}
+
+void Cluster::Query(mmkv::Configuration &config)
+{
+  observer_.Query(config);
+  observer_.Wait();
+}
---
+++ mmkv/cluster/cluster.h
@@ -0,0 +1,27 @@
+#ifndef MMKV_CLUSTER_CLUSTER_H_
+#define MMKV_CLUSTER_CLUSTER_H_
+
+#include "mmkv/tracker/tracker_observer.h"
+#include "mmkv/tracker/tracker.h"
+#include "mmkv/tracker/tracker_client.h"
+
+class Cluster {
+ public:
+  Cluster(EventLoop *loop, InetAddr const &tracker_addr);
+  
+  void StartShardServer(InetAddr const &sharder_addr);
+  void Query(mmkv::Configuration &config);
+
+  void Start();
+ private:
+  mmkv::server::Tracker tracker_;
+  InetAddr tracker_addr_;
+  
+  EventLoopThread tracker_cli_loop_thr_;
+  std::vector<std::unique_ptr<mmkv::server::TrackerClient>> tracker_clis_;
+  
+  EventLoopThread observer_loop_thr_; 
+  mmkv::TrackerObserver observer_;
+};
+
+#endif
