759f9f7fd10c97ce22bec9a66e012490ff3cebeb
Conzxy <1967933844@qq.com>
2023-04-24 22:52:59 +0800

    MOD: use kvarint to codec signed/unsigned

--- mmkv/protocol/mmbp_util.h
+++ mmkv/protocol/mmbp_util.h
@@ -11,6 +11,8 @@
 #include "kanon/buffer/buffer.h"
 #include "mmkv/util/conv.h"
 
+#include <third-party/kvarint/kvarint.h>
+
 namespace mmkv {
 namespace protocol {
 
@@ -26,31 +28,66 @@ using kanon::ChunkList;
    -> ParseComponent()/SerializeComponent()
       (Some non-message struct also follow the MMBP format and don't as a field)
    */
+
+/*--------------------------------------------------*/
+/* Parse API                                        */
+/*--------------------------------------------------*/
+#define MMKV_PARSE_UNSIGNED_(bits_)                                            \
+  size_t len = 0;                                                              \
+  auto err = kvarint_decode##bits_(buffer.GetReadBegin(),                      \
+                                   buffer.GetReadableSize(), &len, &i);        \
+  MMKV_UNUSED(err);                                                            \
+  MMKV_ASSERT1(err == KVARINT_OK);                                             \
+  buffer.AdvanceRead(len)
+
+#define MMKV_PARSE_SIGNED_(bits_)                                              \
+  size_t len = 0;                                                              \
+  auto err = kvarint_decode##bits_##s(buffer.GetReadBegin(),                   \
+                                      buffer.GetReadableSize(), &len, &i);     \
+  MMKV_UNUSED(err);                                                            \
+  MMKV_ASSERT1(err == KVARINT_OK);                                             \
+  buffer.AdvanceRead(len)
+
+MMKV_INLINE void ParseComponent(uint8_t &i, Buffer &buffer)
+{
+  MMKV_PARSE_UNSIGNED_(8);
+}
+
+MMKV_INLINE void ParseComponent(uint16_t &i, Buffer &buffer)
+{
+  MMKV_PARSE_UNSIGNED_(16);
+}
+
+MMKV_INLINE void ParseComponent(uint32_t &i, Buffer &buffer)
+{
+  MMKV_PARSE_UNSIGNED_(32);
+}
+
+MMKV_INLINE void ParseComponent(uint64_t &i, Buffer &buffer)
+{
+  MMKV_PARSE_UNSIGNED_(64);
+}
+
+MMKV_INLINE void ParseComponent(int64_t &i, Buffer &buffer)
+{
+  MMKV_PARSE_SIGNED_(64);
+}
+
 template <typename Alloc>
 MMKV_INLINE void
 ParseComponent(std::basic_string<char, std::char_traits<char>, Alloc> &str,
                Buffer &buffer, bool is_16 = false)
 {
   str.clear();
-  auto len = is_16 ? buffer.Read16() : buffer.Read32();
+  size_t len = -1;
+  if (is_16) {
+    ParseComponent((uint16_t &)len, buffer);
+  } else {
+    ParseComponent((uint32_t &)len, buffer);
+  }
   str.reserve(len);
   str.append(buffer.GetReadBegin(), len);
   buffer.AdvanceRead(len);
-}
-
-MMKV_INLINE void ParseComponent(WeightValues &values, Buffer &buffer)
-{
-  auto count = buffer.Read32();
-  values.resize(count);
-
-  size_t value_size = 0;
-
-  for (size_t i = 0; i < count; ++i) {
-    values[i].key = util::int2double(buffer.Read64());
-    value_size = buffer.Read32();
-    values[i].value.append(buffer.GetReadBegin(), value_size);
-    buffer.AdvanceRead(value_size);
-  }
 }
 
 template <typename Alloc>
@@ -58,75 +95,72 @@ MMKV_INLINE void ParseComponent(
     std::vector<std::basic_string<char, std::char_traits<char>, Alloc>> &values,
     Buffer &buffer)
 {
-  auto count = buffer.Read32();
+  uint32_t count = -1;
+  ParseComponent(count, buffer);
   values.resize(count);
 
   size_t value_size = 0;
 
   for (size_t i = 0; i < count; ++i) {
-    value_size = buffer.Read32();
+    ParseComponent(value_size, buffer);
     values[i].append(buffer.GetReadBegin(), value_size);
+    buffer.AdvanceRead(value_size);
+  }
+}
+
+MMKV_INLINE void ParseComponent(WeightValues &values, Buffer &buffer)
+{
+  uint32_t count = -1;
+  ParseComponent(count, buffer);
+  values.resize(count);
+
+  size_t value_size = 0;
+
+  for (size_t i = 0; i < count; ++i) {
+    values[i].key = util::int2double(buffer.Read64());
+    ParseComponent(value_size, buffer);
+    values[i].value.append(buffer.GetReadBegin(), value_size);
     buffer.AdvanceRead(value_size);
   }
 }
 
 MMKV_INLINE void ParseComponent(StrKvs &kvs, Buffer &buffer)
 {
-  auto count = buffer.Read32();
+  uint32_t count = -1;
+  ParseComponent(count, buffer);
   kvs.resize(count);
 
   size_t key_size = 0;
   size_t value_size = 0;
 
   for (size_t i = 0; i < count; ++i) {
-    key_size = buffer.Read16();
+    ParseComponent(key_size, buffer);
     kvs[i].key.append(buffer.GetReadBegin(), key_size);
     buffer.AdvanceRead(key_size);
-    value_size = buffer.Read32();
+    ParseComponent(value_size, buffer);
     kvs[i].value.append(buffer.GetReadBegin(), value_size);
     buffer.AdvanceRead(value_size);
   }
-}
-
-MMKV_INLINE void ParseComponent(uint8_t &i, Buffer &buffer)
-{
-  i = buffer.Read8();
-}
-
-MMKV_INLINE void ParseComponent(uint16_t &i, Buffer &buffer)
-{
-  i = buffer.Read16();
-}
-
-MMKV_INLINE void ParseComponent(uint32_t &i, Buffer &buffer)
-{
-  i = buffer.Read32();
-}
-
-MMKV_INLINE void ParseComponent(uint64_t &i, Buffer &buffer)
-{
-  i = buffer.Read64();
-}
-
-MMKV_INLINE void ParseComponent(int64_t &i, Buffer &buffer)
-{
-  i = (int64_t)buffer.Read64();
 }
 
 #define SET_FILED_UVEC(bc)                                                     \
   MMKV_INLINE void ParseComponent(std::vector<uint##bc##_t> &u##bc##s,         \
                                   Buffer &buffer)                              \
   {                                                                            \
-    u##bc##s.resize(buffer.Read##bc());                                        \
+    uint##bc##_t size = -1;                                                    \
+    ParseComponent(size, buffer);                                              \
+    u##bc##s.resize(size);                                                     \
     for (auto &u##bc : u##bc##s) {                                             \
-      u##bc = buffer.Read##bc();                                               \
+      ParseComponent(u##bc, buffer);                                           \
     }                                                                          \
   }                                                                            \
                                                                                \
   MMKV_INLINE void ParseComponent(                                             \
       std::vector<std::vector<uint##bc##_t>> &u##bc##_2d, Buffer &buffer)      \
   {                                                                            \
-    u##bc##_2d.resize(buffer.Read##bc());                                      \
+    uint##bc##_t size = -1;                                                    \
+    ParseComponent(size, buffer);                                              \
+    u##bc##_2d.resize(size);                                                   \
     for (auto &u##bc##s : u##bc##_2d) {                                        \
       ParseComponent(u##bc##s, buffer);                                        \
     }                                                                          \
@@ -136,7 +170,7 @@ MMKV_INLINE void ParseComponent(int64_t &i, Buffer &buffer)
 MMKV_INLINE void ParseComponent(uint8_t *u8s, size_t len, Buffer &buffer)
 {
   for (size_t i = 0; i < len; ++i) {
-    u8s[i] = buffer.Read8();
+    ParseComponent(u8s[i], buffer);
   }
 }
 
@@ -145,7 +179,9 @@ SET_FILED_UVEC(32)
 
 MMKV_INLINE void ParseComponent(std::vector<char> &buf, Buffer &buffer)
 {
-  buf.resize(buffer.Read32());
+  uint32_t len = -1;
+  ParseComponent(len, buffer);
+  buf.resize(len);
   memcpy(&buf[0], buffer.GetReadBegin(), buf.size());
   buffer.AdvanceRead(buf.size());
 }
@@ -154,7 +190,8 @@ template <typename K, typename V>
 MMKV_INLINE void ParseComponent(std::unordered_map<K, V> &kv_map,
                                 Buffer &buffer)
 {
-  const auto n = buffer.Read32();
+  uint32_t n = -1;
+  ParseComponent(n, buffer);
   kv_map.reserve(n);
   for (uint32_t i = 0; i < n; ++i) {
     K key;
@@ -165,12 +202,59 @@ MMKV_INLINE void ParseComponent(std::unordered_map<K, V> &kv_map,
   }
 }
 
+/*--------------------------------------------------*/
+/* Serialize API                                    */
+/*--------------------------------------------------*/
+template <typename BT>
+MMKV_INLINE void SerializeComponent(uint8_t i, BT &buffer)
+{
+  kvarint_buf8_t buf;
+  kvarint_encode8(i, &buf);
+  buffer.Append(buf.buf, buf.len);
+}
+
+template <typename BT>
+MMKV_INLINE void SerializeComponent(uint64_t i, BT &buffer)
+{
+  kvarint_buf16_t buf;
+  kvarint_encode16(i, &buf);
+  buffer.Append(buf.buf, buf.len);
+}
+
+template <typename BT>
+MMKV_INLINE void SerializeComponent(uint32_t i, BT &buffer)
+{
+  kvarint_buf32_t buf;
+  kvarint_encode32(i, &buf);
+  buffer.Append(buf.buf, buf.len);
+}
+
+template <typename BT>
+MMKV_INLINE void SerializeComponent(uint16_t i, BT &buffer)
+{
+  kvarint_buf64_t buf;
+  kvarint_encode64(i, &buf);
+  buffer.Append(buf.buf, buf.len);
+}
+
+template <typename BT>
+MMKV_INLINE void SerializeComponent(int64_t i, BT &buffer)
+{
+  kvarint_buf64_t buf;
+  kvarint_encode64s(i, &buf);
+  buffer.Append(buf.buf, buf.len);
+}
+
 template <typename Alloc, typename BT>
 MMKV_INLINE void SerializeComponent(
     std::basic_string<char, std::char_traits<char>, Alloc> const &str,
     BT &buffer, bool is_16 = false)
 {
-  is_16 ? buffer.Append16(str.size()) : buffer.Append32(str.size());
+  if (is_16) {
+    SerializeComponent((uint16_t)str.size(), buffer);
+  } else {
+    SerializeComponent((uint32_t)str.size(), buffer);
+  }
   buffer.Append(str.data(), str.size());
 }
 
@@ -180,10 +264,10 @@ MMKV_INLINE void SerializeComponent(
         &values,
     BT &buffer)
 {
-  buffer.Append32(values.size());
+  SerializeComponent((uint32_t)values.size(), buffer);
 
   for (size_t i = 0; i < values.size(); ++i) {
-    buffer.Append32(values[i].size());
+    SerializeComponent((uint32_t)values[i].size(), buffer);
     buffer.Append(values[i].data(), values[i].size());
   }
 }
@@ -191,12 +275,12 @@ MMKV_INLINE void SerializeComponent(
 template <typename BT>
 MMKV_INLINE void SerializeComponent(StrKvs const &values, BT &buffer)
 {
-  buffer.Append32(values.size());
+  SerializeComponent((uint32_t)values.size(), buffer);
 
   for (size_t i = 0; i < values.size(); ++i) {
-    buffer.Append16(values[i].key.size());
+    SerializeComponent((uint16_t)values[i].key.size(), buffer);
     buffer.Append(values[i].key.data(), values[i].key.size());
-    buffer.Append32(values[i].value.size());
+    SerializeComponent((uint32_t)values[i].value.size(), buffer);
     buffer.Append(values[i].value.data(), values[i].value.size());
   }
 }
@@ -204,37 +288,13 @@ MMKV_INLINE void SerializeComponent(StrKvs const &values, BT &buffer)
 template <typename BT>
 MMKV_INLINE void SerializeComponent(WeightValues const &values, BT &buffer)
 {
-  buffer.Append32(values.size());
+  SerializeComponent((uint32_t)values.size(), buffer);
 
   for (size_t i = 0; i < values.size(); ++i) {
     buffer.Append64(util::double2u64(values[i].key));
-    buffer.Append32(values[i].value.size());
+    SerializeComponent((uint32_t)values[i].value.size(), buffer);
     buffer.Append(values[i].value.data(), values[i].value.size());
   }
-}
-
-template <typename BT>
-MMKV_INLINE void SerializeComponent(uint8_t i, BT &buffer)
-{
-  buffer.Append8(i);
-}
-
-template <typename BT>
-MMKV_INLINE void SerializeComponent(uint64_t i, BT &buffer)
-{
-  buffer.Append64(i);
-}
-
-template <typename BT>
-MMKV_INLINE void SerializeComponent(uint32_t i, BT &buffer)
-{
-  buffer.Append32(i);
-}
-
-template <typename BT>
-MMKV_INLINE void SerializeComponent(uint16_t i, BT &buffer)
-{
-  buffer.Append16(i);
 }
 
 // bc = bit count
@@ -242,7 +302,7 @@ MMKV_INLINE void SerializeComponent(uint16_t i, BT &buffer)
   MMKV_INLINE void SerializeComponent(                                         \
       std::vector<uint##bc##_t> const &u##bc##s, ChunkList &buffer)            \
   {                                                                            \
-    buffer.Append##bc(u##bc##s.size());                                        \
+    SerializeComponent(u##bc##s.size(), buffer);                               \
     for (auto u##bc : u##bc##s)                                                \
       buffer.Append##bc(u##bc);                                                \
   }                                                                            \
@@ -251,7 +311,7 @@ MMKV_INLINE void SerializeComponent(uint16_t i, BT &buffer)
       std::vector<std::vector<uint##bc##_t>> const &u##bc##_2d,                \
       ChunkList &buffer)                                                       \
   {                                                                            \
-    buffer.Append##bc(u##bc##_2d.size());                                      \
+    SerializeComponent(u##bc##_2d.size(), buffer);                             \
     for (auto const &u##bc##s : u##bc##_2d)                                    \
       SerializeComponent(u##bc##s, buffer);                                    \
   }
@@ -262,7 +322,7 @@ SERIALIZED_FIELD_UVEC(16)
 MMKV_INLINE void SerializeComponent(std::vector<char> const &buf,
                                     ChunkList &buffer)
 {
-  buffer.Append32(buf.size());
+  SerializeComponent(buf.size(), buffer);
   buffer.Append(buf.data(), buf.size());
 }
 
@@ -282,13 +342,10 @@ MMKV_INLINE void SerializeComponent(uint8_t const *u8s, size_t len,
                                     ChunkList &buffer)
 {
   for (size_t i = 0; i < len; ++i)
-    buffer.Append32(u8s[i]);
+    SerializeComponent(u8s[i], buffer);
 }
 
-MMKV_INLINE void SetBit(uint8_t &bits, int idx) noexcept
-{
-  bits |= (1 << idx);
-}
+MMKV_INLINE void SetBit(uint8_t &bits, int idx) noexcept { bits |= (1 << idx); }
 
 MMKV_INLINE bool TestBit(uint8_t bits, int idx) noexcept
 {
