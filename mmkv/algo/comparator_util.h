e56889ecd7dda28c99257a83b4296fc21a7f7951
Conzxy <1967933844@qq.com>
2023-05-03 01:16:34 +0800

    feat(Comparator): Complete unsigned and unsigned integers specialization

--- mmkv/algo/comparator_util.h
+++ mmkv/algo/comparator_util.h
@@ -8,38 +8,54 @@
 namespace mmkv {
 namespace algo {
 
-template<typename T>
+template <typename T>
 struct Comparator {
   static_assert(sizeof(T) < 0, "The specialization of Comparator<T> isn't defined");
 };
 
-template<>
-struct Comparator <int> {
-  inline int operator()(int x, int y) const noexcept {
-    return x - y;
+#define MMKV_DEF_COMPARATOR_SPEC_SIGNED_(t_)                                                       \
+  template <>                                                                                      \
+  struct Comparator<t_> {                                                                          \
+    inline int operator()(t_ x, t_ y) const noexcept                                               \
+    {                                                                                              \
+      return x - y;                                                                                \
+    }                                                                                              \
   }
-};
+
+MMKV_DEF_COMPARATOR_SPEC_SIGNED_(int8_t);
+MMKV_DEF_COMPARATOR_SPEC_SIGNED_(int16_t);
+MMKV_DEF_COMPARATOR_SPEC_SIGNED_(int32_t);
+MMKV_DEF_COMPARATOR_SPEC_SIGNED_(int64_t);
+// MMKV_DEF_COMPARATOR_SPEC_SIGNED_(int);
 
 /* To unsigned integer,
  * can't return x - y since
  * (signed)(x - y(y > 2^31-1)) > 0
  */
-template<>
-struct Comparator <uint32_t> {
-  inline int32_t operator()(uint32_t x, uint32_t y) const noexcept {
-    return (x < y) ? -1 : ((x == y) ? 0 : 1);
+#define MMKV_DEF_COMPARATOR_SPEC_UNSIGNED_(t_)                                                     \
+  template <>                                                                                      \
+  struct Comparator<t_> {                                                                          \
+    inline int32_t operator()(t_ x, t_ y) const noexcept                                           \
+    {                                                                                              \
+      return (x < y) ? -1 : ((x == y) ? 0 : 1);                                                    \
+    }                                                                                              \
   }
-};
 
-template<typename Alloc>
-struct Comparator <std::basic_string<char, std::char_traits<char>, Alloc>> {
+MMKV_DEF_COMPARATOR_SPEC_UNSIGNED_(uint8_t);
+MMKV_DEF_COMPARATOR_SPEC_UNSIGNED_(uint16_t);
+MMKV_DEF_COMPARATOR_SPEC_UNSIGNED_(uint32_t);
+MMKV_DEF_COMPARATOR_SPEC_UNSIGNED_(uint64_t);
+
+template <typename Alloc>
+struct Comparator<std::basic_string<char, std::char_traits<char>, Alloc>> {
   using StrType = std::basic_string<char, std::char_traits<char>, Alloc>;
-  inline int operator()(StrType const& x, StrType const& y) const noexcept {
+  inline int operator()(StrType const &x, StrType const &y) const noexcept
+  {
     return ::strcmp(x.c_str(), y.c_str());
   }
 };
 
-} // algo
-} // mmkv
+} // namespace algo
+} // namespace mmkv
 
 #endif // _MMKV_ALGO_COMPARATOR_UTIL_H_
