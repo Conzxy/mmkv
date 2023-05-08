d59dfdbfa5a0c76372d37da7d31bbc031d815fb9
Conzxy <1967933844@qq.com>
2023-05-08 17:09:45 +0800

    feat(ReservedArray): Add non-initialized `Grow()`(`GrowNoInit()`)
    To some use-cases, the reserved array without initialization is required.
    For example, the `DatabaseInstance` array, It don't support move/copy
    operation.
    But I want a array store the multiple instances, the `std::vector<T>`
    does not allow the non-move and non-copy object to emplace or reserve,
    follow the error message from gcc, it will call
    `std::uninitialized_copy()` and it requires the object support copy/move
    operation.
    In my specific container, can avoid this by just allocate memory and
    user can use inplacement new operator to initialize such objects.

--- mmkv/algo/reserved_array.h
+++ mmkv/algo/reserved_array.h
@@ -3,16 +3,16 @@
 #define _MMKV_ALGO_RESERVED_ARRAY_H_
 
 #ifdef DLOG
-#undef DLOG
+#  undef DLOG
 #endif
 
 #ifdef _DEBUG_RESERVED_ARRAY_
-  #include <iostream>
-  #include <stdio.h>
- 
-  #define DLOG(...) printf(__VA_ARGS__);
+#  include <iostream>
+#  include <stdio.h>
+
+#  define DLOG(...) printf(__VA_ARGS__);
 #else
-  #define DLOG(...) 
+#  define DLOG(...)
 #endif
 
 #include <memory>
@@ -35,19 +35,20 @@ namespace algo {
 //    (since reallocate() cannot ensure exception-safe)
 //    and static class variable "can_reallocate = true"
 //************************************************************
-template<typename T, typename=void>
+template <typename T, typename = void>
 struct has_nontype_member_can_reallocate_with_true : std::false_type {};
 
-template<typename T>
-struct has_nontype_member_can_reallocate_with_true<T, zstl::void_t<decltype(&T::can_reallocate)>> : zstl::bool_constant<T::can_reallocate> {};
+template <typename T>
+struct has_nontype_member_can_reallocate_with_true<T, zstl::void_t<decltype(&T::can_reallocate)>>
+  : zstl::bool_constant<T::can_reallocate> {};
 
-template<typename T>
-struct can_reallocate : 
-  zstl::disjunction<
-    std::is_trivial<T>,
-    zstl::conjunction<
-      has_nontype_member_can_reallocate_with_true<T>,
-      std::is_nothrow_default_constructible<T>>> {};
+template <typename T>
+struct can_reallocate
+  : zstl::disjunction<
+        std::is_trivial<T>,
+        zstl::conjunction<
+            has_nontype_member_can_reallocate_with_true<T>,
+            std::is_nothrow_default_constructible<T>>> {};
 
 // template<typename T, typename HasReallocate=std::true_type>
 // struct can_reallocate : std::is_trivial<T> {};
@@ -65,6 +66,15 @@ struct can_reallocate :
 //       zstl::bool_constant<T::can_reallocate>> {};
 
 /**
+ * Determine the whether the Grow() or allocate operation to initialize(default or move) the
+ * elements
+ */
+enum InitFlag : unsigned char {
+  ON  = 1,
+  OFF = 0,
+};
+
+/**
  * \brief Like std::vector<T> but there is no capacity concept
  *
  * 命名由来：
@@ -72,8 +82,8 @@ struct can_reallocate :
  * dynamic_array或scalable_array更为合适，直白并且体现其特征。
  *
  * ReservedArray的命名也体现了其特征：其内容首先进行预分配，然后读取或写入其中的内容。
- * 该容器不支持append，即push/emplace_back等，自然也不支持prepend,即push/emplace_front()等， 故不需要capacity数据成员（或说capacity == size)
- * 支持扩展(Grow)和收缩(Shrink)。
+ * 该容器不支持append，即push/emplace_back等，自然也不支持prepend,即push/emplace_front()等，
+ * 故不需要capacity数据成员（或说capacity == size) 支持扩展(Grow)和收缩(Shrink)。
  *
  * 换言之，ReservedArray只是个默认初始化的内存区域。
  *
@@ -81,64 +91,78 @@ struct can_reallocate :
  * 1) hashtable
  * 2) continuous buffer
  */
-template<typename T, typename Alloc=LibcAllocatorWithRealloc<T>>
+template <typename T, typename Alloc = LibcAllocatorWithRealloc<T>>
 class ReservedArray : protected Alloc {
   using AllocTraits = std::allocator_traits<Alloc>;
-  
+
+// Allow user initialize throught operator[] and inplacement new
+#if 0
   static_assert(std::is_default_constructible<T>::value, 
       "The T(Value) type must be default constructible");
+#endif
+
+// To fixed size reserved array,
+// this is not required.
+#if 0
   static_assert(zstl::disjunction<std::is_move_constructible<T>, std::is_copy_constructible<T>>::value, 
       "The T(Value) type must be move/copy constructible");
+#endif
 
  public:
-  using value_type = T;
-  using reference = T&;
-  using const_reference = T const&;
-  using pointer = T*;
-  using const_pointer = T const*;
-  using size_type = size_t;
-  using iterator = pointer;
-  using const_iterator = const_pointer;
+  using value_type      = T;
+  using reference       = T &;
+  using const_reference = T const &;
+  using pointer         = T *;
+  using const_pointer   = T const *;
+  using size_type       = size_t;
+  using iterator        = pointer;
+  using const_iterator  = const_pointer;
 
   ReservedArray()
     : data_(nullptr)
     , end_(data_)
   {
   }
-  
-  explicit ReservedArray(size_type n) 
+
+  explicit ReservedArray(size_type n, InitFlag flag = InitFlag::ON)
     : data_(AllocTraits::allocate(*this, n))
-    , end_(data_+n)
+    , end_(data_ + n)
   {
     if (data_ == NULL) {
-      throw std::bad_alloc{}; 
+      throw std::bad_alloc{};
     }
-    
+
     try {
-      zstl::UninitializedDefaultConstruct(data_, end_);
-    } catch (...) {
+      if (flag == InitFlag::ON) {
+        zstl::UninitializedDefaultConstruct(data_, end_);
+      }
+    }
+    catch (...) {
       data_ = end_ = nullptr;
       throw;
     }
-  } 
-  
-  ReservedArray(ReservedArray const& other)
-    : data_(AllocTraits::allocate(*this, other.size()))
-    , end_(data_+other.size())
+  }
+
+  ReservedArray(ReservedArray const &other)
+    : Alloc()
+    , data_(AllocTraits::allocate(*this, other.size()))
+    , end_(data_ + other.size())
   {
     try {
       std::uninitialized_copy(other.begin(), other.end(), data_);
-    } catch (...) {
+    }
+    catch (...) {
       AllocTraits::deallocate(*this, data_, other.size());
       data_ = end_ = nullptr;
       throw;
     }
-  } 
-  
-  ReservedArray& operator=(ReservedArray const& other) {
+  }
+
+  ReservedArray &operator=(ReservedArray const &other)
+  {
     if (&other == this) {
       return *this;
-    } 
+    }
 
     if (size() == other.size()) {
       // 为了保证exception-safe：
@@ -152,124 +176,142 @@ class ReservedArray : protected Alloc {
     return *this;
   }
 
-  ReservedArray(ReservedArray&& other) noexcept 
+  ReservedArray(ReservedArray &&other) noexcept
     : data_(other.data_)
-    , end_(other.end_) {
+    , end_(other.end_)
+  {
     other.data_ = other.end_ = nullptr;
   }
 
-  ReservedArray& operator=(ReservedArray&& other) noexcept {
+  ReservedArray &operator=(ReservedArray &&other) noexcept
+  {
     this->swap(other);
     return *this;
   }
 
-  ~ReservedArray() noexcept {
+  ~ReservedArray() noexcept
+  {
     if (end_ != data_) {
       zstl::DestroyRange(data_, end_);
       AllocTraits::deallocate(*this, data_, size());
     }
     data_ = end_ = nullptr;
   }
-  
-  void Grow(size_type n) {
-    if (n <= GetSize()) { return; }
+
+  void Grow(size_type n)
+  {
+    if (n <= GetSize()) {
+      return;
+    }
 
     Reallocate<value_type>(n);
   }
 
+  void GrowNoInit(size_type n) { new (this) ReservedArray(n); }
+
   /**
    * To user-defined can_reallocate class,
    */
-  void Shrink(size_type n) {
-    if (n >= GetSize()) { return; }
+  void Shrink(size_type n)
+  {
+    if (n >= GetSize()) {
+      return;
+    }
 
     Shrink_impl<value_type>(n);
   }
 
   size_type GetSize() const noexcept { return end_ - data_; }
   size_type size() const noexcept { return end_ - data_; }
-  bool empty() const noexcept { return data_ == end_; }  
+  bool      empty() const noexcept { return data_ == end_; }
 
-  reference operator[](size_type i) noexcept {
+  reference operator[](size_type i) noexcept
+  {
     assert(i < size());
 
     return data_[i];
   }
 
-  const_reference operator[](size_type i) const noexcept {
+  const_reference operator[](size_type i) const noexcept
+  {
     assert(i < size());
     return data_[i];
   }
-  
-  iterator begin() noexcept { return data_; }  
-  iterator end() noexcept { return end_; }
+
+  iterator       begin() noexcept { return data_; }
+  iterator       end() noexcept { return end_; }
   const_iterator begin() const noexcept { return data_; }
   const_iterator end() const noexcept { return end_; }
   const_iterator cbegin() const noexcept { return data_; }
   const_iterator cend() const noexcept { return end_; }
-  
-  pointer data() noexcept { return data_; }
-  const_pointer data() const noexcept { return data_; } 
 
-  void swap(ReservedArray& other) noexcept {
+  pointer       data() noexcept { return data_; }
+  const_pointer data() const noexcept { return data_; }
+
+  void swap(ReservedArray &other) noexcept
+  {
     std::swap(data_, other.data_);
     std::swap(end_, other.end_);
   }
 
  private:
-  template<typename U, zstl::enable_if_t<can_reallocate<U>::value, int> =0>
-  void Reallocate(size_type n) {
-      // Failed to call the reallocate(),
-      // the old memory block is not freed
-      DLOG("size(before realloc): %zu\n", size());
-      DLOG("%p\n", this);
-      auto tmp = this->reallocate(data_, size(), n);
+  template <typename U, zstl::enable_if_t<can_reallocate<U>::value, int> = 0>
+  void Reallocate(size_type n)
+  {
+    // Failed to call the reallocate(),
+    // the old memory block is not freed
+    DLOG("size(before realloc): %zu\n", size());
+    DLOG("%p\n", this);
+    auto tmp = this->reallocate(data_, size(), n);
 
-      if (tmp == NULL) {
-        throw std::bad_alloc{};
-      }
-      
-      // 为了支持包含can_reallocate = true的non-trivial类类型也能进行reallocate
-      // 对扩展的内存区域进行默认初始化 
-      
-      // reallocate无法保证异常安全
-      // 因为data_的内存区域可能已被释放 
-      DLOG("default consturct [%zu, %zu)\n", size(), n);
-      zstl::UninitializedDefaultConstruct(tmp+size(), tmp+n);
+    if (tmp == NULL) {
+      throw std::bad_alloc{};
+    }
 
-      data_ = tmp;
-      end_ = data_ + n;
+    // 为了支持包含can_reallocate = true的non-trivial类类型也能进行reallocate
+    // 对扩展的内存区域进行默认初始化
+
+    // reallocate无法保证异常安全
+    // 因为data_的内存区域可能已被释放
+    DLOG("default consturct [%zu, %zu)\n", size(), n);
+    zstl::UninitializedDefaultConstruct(tmp + size(), tmp + n);
+
+    data_ = tmp;
+    end_  = data_ + n;
   }
 
-  template<typename U, zstl::enable_if_t<!can_reallocate<U>::value, char> =0>
-  void Reallocate(size_type n) {
-      // To ensure exception-safe,
-      // set data_ and end_ at last
-      auto new_data = AllocTraits::allocate(*this, n);
-      if (new_data == NULL) {
-        throw std::bad_alloc{};
-      } 
-  
-      auto new_end = new_data;
+  template <typename U, zstl::enable_if_t<!can_reallocate<U>::value, char> = 0>
+  void Reallocate(size_type n)
+  {
+    // To ensure exception-safe,
+    // set data_ and end_ at last
+    auto new_data = AllocTraits::allocate(*this, n);
+    if (new_data == NULL) {
+      throw std::bad_alloc{};
+    }
 
-      try {
-        new_end = zstl::UninitializedMoveIfNoexcept(data_, end_, new_end);
-        new_end = zstl::UninitializedDefaultConstruct(new_end, new_data+n);
-      } catch (...) {
-        AllocTraits::deallocate(*this, new_data, n);
-        throw;
-      }
+    auto new_end = new_data;
 
-      AllocTraits::deallocate(*this, data_, GetSize());
-      data_ = new_data;
-      end_ = new_end;
+    try {
+      new_end = zstl::UninitializedMoveIfNoexcept(data_, end_, new_end);
+      new_end = zstl::UninitializedDefaultConstruct(new_end, new_data + n);
+    }
+    catch (...) {
+      AllocTraits::deallocate(*this, new_data, n);
+      throw;
+    }
+
+    AllocTraits::deallocate(*this, data_, GetSize());
+    data_ = new_data;
+    end_  = new_end;
   }
 
-  template<typename U, zstl::enable_if_t<can_reallocate<U>::value, char> =0>
-  void Shrink_impl(size_type n) {
+  template <typename U, zstl::enable_if_t<can_reallocate<U>::value, char> = 0>
+  void Shrink_impl(size_type n)
+  {
     // destroy一般来说是no throw的
     DLOG("destroy: [%zu, %zu)\n", n, size());
-    zstl::DestroyRange(data_+n, end_);
+    zstl::DestroyRange(data_ + n, end_);
     auto tmp = this->reallocate(data_, size(), n);
 
     if (tmp == NULL && n != 0) {
@@ -280,25 +322,27 @@ class ReservedArray : protected Alloc {
       data_ = end_ = nullptr;
     } else {
       data_ = tmp;
-      end_ = data_+n;
+      end_  = data_ + n;
     }
   }
 
-  template<typename U, zstl::enable_if_t<!can_reallocate<U>::value, int> =0>
-  void Shrink_impl(size_type n) {
+  template <typename U, zstl::enable_if_t<!can_reallocate<U>::value, int> = 0>
+  void Shrink_impl(size_type n)
+  {
     pointer new_data = nullptr;
-    pointer new_end = nullptr;
+    pointer new_end  = nullptr;
     if (n != 0) {
       new_data = AllocTraits::allocate(*this, n);
       if (new_data == NULL) {
         throw std::bad_alloc{};
-      } 
+      }
 
       new_end = new_data;
 
       try {
-        new_end = zstl::UninitializedMoveIfNoexcept(data_, data_+n, new_end);
-      } catch (...) {
+        new_end = zstl::UninitializedMoveIfNoexcept(data_, data_ + n, new_end);
+      }
+      catch (...) {
         AllocTraits::deallocate(*this, new_data, n);
         throw;
       }
@@ -311,26 +355,32 @@ class ReservedArray : protected Alloc {
     AllocTraits::deallocate(*this, data_, GetSize());
 
     data_ = new_data;
-    end_ = new_end;
-  }
-  
-  template<typename U, typename=zstl::enable_if_t<std::is_nothrow_copy_assignable<U>::value>>
-  void CopyAssignmentForSizeEqual(ReservedArray const& other) {
-    std::copy(other.begin(), other.end(), begin()); 
+    end_  = new_end;
   }
 
-  template<typename U, zstl::enable_if_t<!std::is_nothrow_copy_assignable<U>::value, int> =0>
-  void CopyAssignmentForSizeEqual(ReservedArray const& other) {
+  template <typename U, typename = zstl::enable_if_t<std::is_nothrow_copy_assignable<U>::value>>
+  void CopyAssignmentForSizeEqual(ReservedArray const &other)
+  {
+    std::copy(other.begin(), other.end(), begin());
+  }
+
+  template <typename U, zstl::enable_if_t<!std::is_nothrow_copy_assignable<U>::value, int> = 0>
+  void CopyAssignmentForSizeEqual(ReservedArray const &other)
+  {
     // swap是no throw的，因此如果拷贝构造抛出了异常
     // 不会导致this被改动（或破坏）
     ReservedArray(other).swap(*this);
   }
-  
-  template<typename U> 
-  using cond = zstl::conjunction<can_reallocate<U>, std::is_nothrow_copy_assignable<U>, std::is_nothrow_constructible<U>>;
 
-  template<typename U, typename=zstl::enable_if_t<cond<U>::value>>
-  void CopyAssignmentForSizeNotEqual(ReservedArray const& other) {
+  template <typename U>
+  using cond = zstl::conjunction<
+      can_reallocate<U>,
+      std::is_nothrow_copy_assignable<U>,
+      std::is_nothrow_constructible<U>>;
+
+  template <typename U, typename = zstl::enable_if_t<cond<U>::value>>
+  void CopyAssignmentForSizeNotEqual(ReservedArray const &other)
+  {
     auto tmp = this->reallocate(data_, size(), other.size());
 
     if (tmp == NULL) {
@@ -338,53 +388,58 @@ class ReservedArray : protected Alloc {
     }
 
     if (other.size() > size()) {
-      std::copy(other.begin(), other.begin()+size(), tmp);
-      std::uninitialized_copy(other.begin()+size(), other.end(), tmp+size());
+      std::copy(other.begin(), other.begin() + size(), tmp);
+      std::uninitialized_copy(other.begin() + size(), other.end(), tmp + size());
     } else {
       std::copy(other.begin(), other.end(), tmp);
-      zstl::DestroyRange(tmp+other.size(), tmp+size());
+      zstl::DestroyRange(tmp + other.size(), tmp + size());
     }
 
     data_ = tmp;
-    end_ = data_ + other.size();
-  } 
+    end_  = data_ + other.size();
+  }
 
-  template<typename U, zstl::enable_if_t<!cond<U>::value, int> =0>
-  void CopyAssignmentForSizeNotEqual(ReservedArray const& other) {
+  template <typename U, zstl::enable_if_t<!cond<U>::value, int> = 0>
+  void CopyAssignmentForSizeNotEqual(ReservedArray const &other)
+  {
     auto data = AllocTraits::allocate(*this, other.size());
-  
+
     if (data == NULL) {
       throw std::bad_alloc{};
     }
 
     try {
       std::uninitialized_copy(other.begin(), other.end(), data);
-    } catch (...) {
+    }
+    catch (...) {
       AllocTraits::deallocate(*this, data, other.size());
       throw;
     }
-    
-    zstl::DestroyRange(data_, end_); 
+
+    zstl::DestroyRange(data_, end_);
     AllocTraits::deallocate(*this, data_, size());
 
     data_ = data;
-    end_ = data + other.size();
-  } 
+    end_  = data + other.size();
+  }
 
-  T* data_;
-  T* end_;
+  T *data_;
+  T *end_;
 };
 
-} // algo
-} // mmkv
+} // namespace algo
+} // namespace mmkv
 
 namespace std {
 
-template<typename T>
-inline void swap(mmkv::algo::ReservedArray<T>& x, mmkv::algo::ReservedArray<T>& y) noexcept(noexcept(x.swap(y))) {
+template <typename T>
+inline void swap(mmkv::algo::ReservedArray<T> &x, mmkv::algo::ReservedArray<T> &y) noexcept(
+    noexcept(x.swap(y))
+)
+{
   x.swap(y);
 }
 
-} // std
+} // namespace std
 
 #endif // _MMKV_ALGO_RESERVED_ARRAY_H_
