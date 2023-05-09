3ecdb3fc745582ed28b9e7a70d2c6fe72e08ddd7
Conzxy <1967933844@qq.com>
2023-05-09 12:41:52 +0800

    feat(shard_util): Overload to accpet `kanon::StringView`

--- mmkv/util/shard_util.h
+++ mmkv/util/shard_util.h
@@ -3,23 +3,27 @@
 #define MMKV_UTIL_SHARD_UTIL_H_
 
 #include "mmkv/protocol/type.h"
+#include "mmkv/util/macro.h"
 
 #include <xxhash.h>
 
 #define DEFAULT_SHARD_NUM (1 << 12) /** Default number of shard */
-#define SHARD_TEST 1
+#define SHARD_TEST        1
 
 namespace mmkv {
 
 using protocol::Shard;
 using protocol::String;
 
-template<typename Alloc>
-inline Shard MakeShardId(std::basic_string<char, std::char_traits<char> , Alloc> const &key) noexcept {
-  return XXH32(key.c_str(), key.size(), 0);
-
+template <typename Alloc>
+MMKV_INLINE Shard MakeShardId(std::basic_string<char, std::char_traits<char>, Alloc> const &key
+) noexcept
+{
+  return XXH64(key.c_str(), key.size(), 0);
 }
 
-} // mmkv
+MMKV_INLINE Shard MakeShardId(StringView key) noexcept { return XXH64(key.data(), key.size(), 0); }
+
+} // namespace mmkv
 
 #endif // MMKV_UTIL_SHARD_UTIL_H_
