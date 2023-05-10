c9819882a78f8bfe9d8ef4c114fa84dfb8dd5d72
Conzxy <1967933844@qq.com>
2023-05-10 11:46:42 +0800

    refactor(MmkvData): Support move and dtor to replace by-hand delete func

--- mmkv/db/mmkv_data.cc
+++ mmkv/db/mmkv_data.cc
@@ -9,25 +9,26 @@ using namespace mmkv::algo;
 namespace mmkv {
 namespace db {
 
-void DeleteMmkvData(MmkvData &data) {
+void DeleteMmkvData(MmkvData &data)
+{
   switch (data.type) {
     case D_STRING:
-      delete (String*)data.any_data;
+      delete (String *)data.any_data;
       break;
     case D_STRLIST:
-      delete (StrList*)data.any_data;
+      delete (StrList *)data.any_data;
       break;
     case D_SORTED_SET:
-      delete (Vset*)data.any_data;
+      delete (Vset *)data.any_data;
       break;
     case D_MAP:
-      delete (Map*)data.any_data;
+      delete (Map *)data.any_data;
       break;
     case D_SET:
-      delete (Set*)data.any_data;
+      delete (Set *)data.any_data;
       break;
   }
 }
 
-} // db
-} // mmkv
+} // namespace db
+} // namespace mmkv
--- mmkv/db/mmkv_data.h
+++ mmkv/db/mmkv_data.h
@@ -2,14 +2,40 @@
 #ifndef _MMKV_ALGO_MMKV_DATA_H_
 #define _MMKV_ALGO_MMKV_DATA_H_
 
+#include <utility>
+
 #include "data_type.h"
+#include "mmkv/util/macro.h"
 
 namespace mmkv {
 namespace db {
 
 struct MmkvData {
-  DataType type;  /** Explain the any_data */
-  void* any_data; /** Store any data type */
+  DataType type;     /** Explain the any_data */
+  void    *any_data; /** Store any data type */
+
+  explicit MmkvData(DataType type_)
+    : type(type_)
+    // In most case, nullptr used as dummy data(If data does exists, don't fill it)
+    , any_data(nullptr)
+  {
+  }
+
+  MMKV_INLINE MmkvData(MmkvData &&oth) noexcept
+    : type(oth.type)
+    , any_data(oth.any_data)
+  {
+    oth.any_data = nullptr;
+  }
+
+  MMKV_INLINE MmkvData &operator=(MmkvData &&oth) noexcept
+  {
+    std::swap(type, oth.type);
+    std::swap(any_data, oth.any_data);
+    return *this;
+  }
+
+  MMKV_INLINE ~MmkvData() noexcept;
 };
 
 /* This should be the destructor of MMkvData,
@@ -20,11 +46,26 @@ struct MmkvData {
  * > No need to check if any_data is nullptr
  * Cons:
  * > Need call it by hand
- * But Only fwe commands need, so it is acceptable.
+ * But Only few commands need, so it is acceptable.
+ *
+ *
+ * \warning Deprecated now
  */
 void DeleteMmkvData(MmkvData &data);
 
-} // db 
-} // mmkv
+template <typename T>
+MMKV_INLINE void DeleteSpecificMmkvData(MmkvData *p_data)
+{
+  delete (T *)(p_data->any_data);
+  p_data->any_data = nullptr;
+}
+
+MmkvData::~MmkvData() noexcept
+{
+  if (any_data) DeleteMmkvData(*this);
+}
+
+} // namespace db
+} // namespace mmkv
 
 #endif // _MMKV_ALGO_MMKV_DATA_H_
