ac8f921786215f80006a4c48765ec9713fbd25e3
Conzxy <1967933844@qq.com>
2023-06-01 02:46:10 +0800

    chore: format

--- mmkv/replacement/lru_cache.h
+++ mmkv/replacement/lru_cache.h
@@ -14,39 +14,44 @@ namespace replacement {
 using algo::Blist;
 using algo::Dictionary;
 
-template<typename K>
+template <typename K>
 class LruCache : public CacheInterface<K> {
-  using Cache = Blist<K*, algo::LibcAllocatorWithReallocNoRecord<K*>>;
-  using Dict = Dictionary<K, typename Cache::Node*, algo::Hash<K>, algo::EqualKey<K>, algo::LibcAllocatorWithReallocNoRecord<K>>;
+  using Cache = Blist<K *, algo::LibcAllocatorWithReallocNoRecord<K *>>;
+  using Dict  = Dictionary<
+      K,
+      typename Cache::Node *,
+      algo::Hash<K>,
+      algo::EqualKey<K>,
+      algo::LibcAllocatorWithReallocNoRecord<K>>;
 
  public:
-  explicit LruCache(size_t max_size) noexcept 
+  explicit LruCache(size_t max_size) noexcept
     : CacheInterface<K>()
-    , max_size_(max_size) {
+    , max_size_(max_size)
+  {
   }
 
   ~LruCache() noexcept = default;
 
   // average: O(1)
-  auto UpdateEntry(K const &entry) -> K* override { return UpdateEntry_(entry); }
-  auto UpdateEntry(K &&entry) -> K* override { return UpdateEntry_(std::move(entry)); }
+  auto UpdateEntry(K const &entry) -> K * override { return UpdateEntry_(entry); }
+  auto UpdateEntry(K &&entry) -> K * override { return UpdateEntry_(std::move(entry)); }
 
-  template<typename U>
-  auto UpdateEntry_(U &&entry) -> K*;
+  template <typename U>
+  auto UpdateEntry_(U &&entry) -> K *;
 
   // average: O(1)
   auto DelEntry(K const &key) -> bool override;
 
   // average: O(1)
-  auto Search(K const &key) -> K* override;
+  auto Search(K const &key) -> K * override;
 
-  // O(1)  
-  auto Victim() -> K* override {
-    return (cache_.empty()) ? nullptr : cache_.Back();
-  }
+  // O(1)
+  auto Victim() -> K * override { return (cache_.empty()) ? nullptr : cache_.Back(); }
 
-  // average: O(1) 
-  auto DelVictim() -> void override {
+  // average: O(1)
+  auto DelVictim() -> void override
+  {
     if (cache_.empty()) return;
     dict_.Erase(*cache_.Back());
     cache_.PopBack();
@@ -55,17 +60,17 @@ class LruCache : public CacheInterface<K> {
   auto size() const noexcept -> size_t override { return cache_.size(); }
   auto max_size() const noexcept -> size_t override { return max_size_; }
   auto Clear() -> void override;
-  auto New() const -> LruCache* override { return new LruCache(max_size_); }
-  auto entries() const noexcept -> Cache const& { return cache_; }  
+  auto New() const -> LruCache * override { return new LruCache(max_size_); }
+  auto entries() const noexcept -> Cache const & { return cache_; }
 
  private:
   size_t max_size_; /** The maxsize of cache */
-  Cache cache_;     /** To implement the LRU policy */
-  Dict dict_;       /** For searching */
+  Cache  cache_;    /** To implement the LRU policy */
+  Dict   dict_;     /** For searching */
 };
 
-} // replacement
-} // mmkv
+} // namespace replacement
+} // namespace mmkv
 
 #include "internal/lru_cache_impl.h"
 
